9c544f18eb7a5e5d955c8f55e55a179edee4e05b
vikram <vspawar056@gmail.com>
2020-09-07 14:49:57 +0530

    i am a new to this

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const api = {
 
 const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keypress", setQuery);
-getResults(searchbox.value);
 function setQuery(evt) {
   if (evt.keyCode == 13) {
     getResults(searchbox.value);
